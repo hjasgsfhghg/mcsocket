8747c0f92e84d99007e8b31957793bbf5642c0af
hjasgsfhghg <132178463+hjasgsfhghg@users.noreply.github.com>
2024-09-18 09:03:52 +0800

    Update socket.js

--- mcjswebsocket/socket.js
+++ mcjswebsocket/socket.js
@@ -1,42 +1,58 @@
 const WebSocket = require('ws')
 const uuid = require('uuid')
 
-// Create a new websocket server on port 3000
-console.log('Ready. On MineCraft chat, type /connect localhost:3000')
-const wss = new WebSocket.Server({ port: 3003 })
 
-wss.on('connection', socket => {
+const ws = new WebSocket.Server({ port: 3003 })
+console.log('启动')
+ws.on('connection', socket => {
   console.log('Connected')
 
-  // Tell Minecraft to send all chat messages. Required once after Minecraft starts
+
+
   socket.send(JSON.stringify({
     "header": {
-      "version": 1,                   
-      "requestId": uuid.v4(),          
-      "messageType": "commandRequest",  
-      "messagePurpose": "subscribe"     
+      "version": 1,
+      "requestId": uuid.v4(),
+      "messageType": "commandRequest",
+      "messagePurpose": "subscribe"
     },
     "body": {
-      "eventName": "PlayerMessage"     
+      "eventName": "PlayerMessage"
     },
   }))
+
   socket.send(JSON.stringify({
     "header": {
-      "version": 1,                   
-      "requestId": uuid.v4(),          
-      "messageType": "commandRequest",  
-      "messagePurpose": "subscribe"     
+      "version": 1,
+      "requestId": uuid.v4(),
+      "messageType": "commandRequest",
+      "messagePurpose": "subscribe"
     },
     "body": {
-      "eventName": "ScreenHeartBeat"     
+      "eventName": "ScreenHeartBeat"
     },
   }))
-  // When MineCraft sends a message (e.g. on player chat), print it.
-  socket.on('message', packet => {
-    const msg = JSON.parse(packet)
-    console.log(msg)
+
+  socket.on('message', data => {
+    console.log(data.toString())
+    const msg = JSON.parse(data.toString())
+
+    console.log(msg.header.eventName)
+    if (msg.header.eventName == 'PlayerMessage') {
+
+      ws.clients.forEach(man => {
+
+        if (man.id == 'bot') {
+          console.log('有')
+          man.send(JSON.stringify(msg))
+        }
+      })
+    } else if (msg.header.eventName == 'botid') {
+      socket.id = 'bot'
+    }
   })
-    socket.on('update', packet => {
+
+  socket.on('update', packet => {
     const msg = JSON.parse(packet)
     console.log(msg)
   })
